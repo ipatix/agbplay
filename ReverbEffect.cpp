8b070bad43f38900600b1fbaf361940080a1dd2f
ipatix <ipatix@users.noreply.github.com>
2015-12-29 18:07:35 +0100

    Finished implementation of default agb reverb algorithm

--- ReverbEffect.cpp
+++ ReverbEffect.cpp
@@ -1,20 +1,100 @@
+#include <algorithm>
+
 #include "ReverbEffect.h"
 
 using namespace agbplay;
+using namespace std;
 
 /*
  * public ReverbEffect
  */
 
-ReverbEffect::ReverbEffect(int intensity, uint32_t streamRate, uint32_t agbRate, uint8_t numAgbBuffers)
+ReverbEffect::ReverbEffect(int intensity, uint32_t streamRate, uint8_t numAgbBuffers)
 {
-    this->intensity = intensity;
+    this->intensity = 0.0f;
     if (intensity <= 0) {
-        rtype = RevType::NONE;
+        this->rtype = RevType::NONE;
     } else if (intensity <= 0x7F) {
-        rtype = RevType::NORMAL;
+        this->rtype = RevType::NORMAL;
+        this->intensity = (float)intensity / 128.0f;
+        // TODO check if this value should be devided by 256.0f
     } else {
         rtype = RevType::GS;
     }
+    uint32_t bufferLen = streamRate / AGB_FPS;
+    this->reverbBuffer = new vector<float>(bufferLen * N_CHANNELS * numAgbBuffers);
+    fill(reverbBuffer->begin(), reverbBuffer->end(), 0.0f);
+    this->bufferPos = 0;
+    this->bufferPos2 = bufferLen;
+}
 
+ReverbEffect::~ReverbEffect()
+{
+    delete reverbBuffer;
+}
+
+void ReverbEffect::ProcessData(float *buffer, uint32_t amount)
+{
+    switch (rtype) {
+        case RevType::NONE:
+            break;
+        case RevType::NORMAL:
+            while (amount > 0) {
+                uint32_t left = processNormal(buffer, amount);
+                buffer += (amount - left) * N_CHANNELS;
+                amount = left;
+            }
+            break;
+        case RevType::GS:
+            while (amount > 0) {
+                uint32_t left = processGS(buffer, amount);
+                buffer += (amount - left) * N_CHANNELS;
+                amount = left;
+            }
+            break;
+    }
+}
+
+uint32_t ReverbEffect::processNormal(float *buffer, uint32_t amount)
+{
+    vector<float>& rbuf = *reverbBuffer;
+    uint32_t count;
+    bool reset = false, reset2 = false;
+    if (getBlocksPerBuffer() - bufferPos2 < amount) {
+        count = getBlocksPerBuffer() - bufferPos2;
+        reset2 = true;
+    } else if (getBlocksPerBuffer() - bufferPos < amount) {
+        count = getBlocksPerBuffer() - bufferPos;
+        reset = true;
+    } else {
+        count = amount;
+    }
+    uint32_t ipos = 0;
+    for (uint32_t c = count; c > 0; c--) 
+    {
+        float rev = (rbuf[bufferPos * 2] + rbuf[bufferPos * 2 + 1] + 
+                rbuf[bufferPos2 * 2] + rbuf[bufferPos2 * 2 + 1]) * intensity / 4.0f ;
+        float oldl = buffer[ipos];
+        buffer[ipos++] += rev;
+        float oldr = buffer[ipos];
+        buffer[ipos++] += rev;
+        rbuf[bufferPos * 2] = oldl;
+        rbuf[bufferPos++ * 2 + 1] = oldr;
+    }
+    if (reset2) bufferPos2 = 0;
+    else if (reset) bufferPos = 0;
+    return amount - count;
+}
+
+uint32_t ReverbEffect::processGS(float *buffer, uint32_t amount)
+{
+    (void)buffer;
+    (void)amount;
+    // TODO implement
+    return 0;
+}
+
+uint32_t ReverbEffect::getBlocksPerBuffer()
+{
+    return uint32_t(reverbBuffer->size() / N_CHANNELS);
 }
--- ReverbEffect.h
+++ ReverbEffect.h
@@ -1,18 +1,28 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
+
+#define AGB_FPS 60
+#define N_CHANNELS 2
 
 namespace agbplay
 {
     class ReverbEffect
     {
         public:
-            ReverbEffect(int intesity, uint32_t streamRate, uint32_t agbRate, uint8_t numAgbBuffers);
+            ReverbEffect(int intesity, uint32_t streamRate, uint8_t numAgbBuffers);
             ~ReverbEffect();
             void ProcessData(float *buffer, uint32_t amount);
         private:
+            uint32_t processNormal(float *buffer, uint32_t amount);
+            uint32_t processGS(float *buffer, uint32_t amount);
+            uint32_t getBlocksPerBuffer();
             enum class RevType { NONE, NORMAL, GS } rtype;
-            int intensity;
+            float intensity;
             uint32_t streamRate;
+            std::vector<float> *reverbBuffer;
+            uint32_t bufferPos;
+            uint32_t bufferPos2;
     };
 }

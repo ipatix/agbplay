bd71c032d7d08bb616fa420ce9efecff3e6cf564
ipatix <ipatix@users.noreply.github.com>
2016-04-20 15:38:30 +0200

    Did some more minor adjustments to reverb testing

--- src/ReverbEffect.cpp
+++ src/ReverbEffect.cpp
@@ -119,10 +119,10 @@ uint32_t ReverbEffect::processGS2(float *buffer, uint32_t nBlocks)
         float in_delay_1_l = rbuf[bufferPos * 2], in_delay_1_r = rbuf[bufferPos * 2 + 1];
         float in_delay_2_l = rbuf[bufferPos2 * 2], in_delay_2_r = rbuf[bufferPos2 * 2 + 1];
 
-        delay1HPcarryL = (in_delay_1_l + delay1HPcarryL - delay1HPprevL) * 0.15f;
-        delay1HPcarryR = (in_delay_1_r + delay1HPcarryR - delay1HPprevR) * 0.15f;
-        delay2HPcarryL = (in_delay_2_l + delay2HPcarryL - delay2HPprevL) * 0.15f;
-        delay2HPcarryR = (in_delay_2_r + delay2HPcarryR - delay2HPprevR) * 0.15f;
+        delay1HPcarryL = (in_delay_1_l + delay1HPcarryL - delay1HPprevL) * 0.95f;
+        delay1HPcarryR = (in_delay_1_r + delay1HPcarryR - delay1HPprevR) * 0.95f;
+        delay2HPcarryL = (in_delay_2_l + delay2HPcarryL - delay2HPprevL) * 0.95f;
+        delay2HPcarryR = (in_delay_2_r + delay2HPcarryR - delay2HPprevR) * 0.95f;
 
         delay1HPprevL = in_delay_1_l;
         delay1HPprevR = in_delay_1_r;
@@ -134,8 +134,8 @@ uint32_t ReverbEffect::processGS2(float *buffer, uint32_t nBlocks)
         in_delay_2_l = delay2HPcarryL;
         in_delay_2_r = delay2HPcarryR;
 
-        float r_left = in_delay_1_r * (3.0f / 8.0f) + in_delay_2_l * (3.0f / 8.0f);
-        float r_right = -in_delay_1_l * (3.0f / 8.0f) - in_delay_2_r * (3.0f / 8.0f);
+        float r_left = in_delay_1_r * (4.0f / 8.0f) + in_delay_2_l * (4.0f / 8.0f);
+        float r_right = -in_delay_1_l * (4.0f / 8.0f) - in_delay_2_r * (4.0f / 8.0f);
         
         //__print_debug(FormatString("l=%f, r=%f", r_left, r_right));
         rbuf[bufferPos * 2] = *buffer++ += r_left;

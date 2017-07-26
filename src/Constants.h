54b005e93a82417d002df7b7898f6117827eac04
ipatix <ipatix@users.noreply.github.com>
2017-07-26 23:55:35 +0200

    add logic to prevent buffer jams on higher samplerates

--- src/Constants.h
+++ src/Constants.h
@@ -9,7 +9,15 @@
 
 #define N_CHANNELS 2
 #define STREAM_SAMPLERATE 48000
-#define STREAM_BUF_SIZE 2048
+
+#define __STRM_BPSM ((STREAM_SAMPLERATE / 24) - 1)
+#define __STRM_BSA (__STRM_BPSM | (__STRM_BPSM >> 1))
+#define __STRM_BSB (__STRM_BSA | (__STRM_BSA >> 2))
+#define __STRM_BSC (__STRM_BSB | (__STRM_BSB >> 4))
+#define __STRM_BSD (__STRM_BSC | (__STRM_BSC >> 8))
+#define __STRM_BSE (__STRM_BSD | (__STRM_BSD >> 16))
+
+#define STREAM_BUF_SIZE (__STRM_BSE+1)
 
 #define WINDOW_MIN_WIDTH 80
 #define WINDOW_MIN_HEIGHT 24

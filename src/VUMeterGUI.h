b94a2fdd712e05c5e7d3a71d1104cccf2b4550a6
Michael Panzlaff <michael.panzlaff@fau.de>
2020-12-02 01:21:14 +0100

    VUMeterGUI: add missing override

--- src/VUMeterGUI.h
+++ src/VUMeterGUI.h
@@ -7,10 +7,10 @@ class VUMeterGUI : public CursesWin
 public:
     VUMeterGUI(uint32_t height, uint32_t width, uint32_t yPos, uint32_t xPos);
     ~VUMeterGUI() override;
-    void Resize(uint32_t height, uint32_t width, uint32_t yPos, uint32_t xPos);
+    void Resize(uint32_t height, uint32_t width, uint32_t yPos, uint32_t xPos) override;
     void SetVol(float left, float right);
 private:
-    void update();
+    void update() override;
 
     float vuLevelLeft;
     float vuLevelRight;

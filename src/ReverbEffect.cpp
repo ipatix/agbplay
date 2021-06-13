ipatix/agbplay

commit d3ea650e8413ef07ff602fee77bb30d35d2642c2

refactor a bunch of things
This is an attempt to refactor a bunch of things in agbplay.
Making data structured more logical, make adding features
hopefully easier, adding polyphony to CGB channels and
a few other things.

src/ReverbEffect.cpp
@@ -10,9 +10,9 @@
  */
 
 ReverbEffect::ReverbEffect(uint8_t intensity, size_t streamRate, uint8_t numAgbBuffers)
-    : reverbBuffer((streamRate / AGB_FPS) * N_CHANNELS * numAgbBuffers, 0.f)
+    : reverbBuffer((streamRate / AGB_FPS) * numAgbBuffers, sample{0.0f, 0.0f})
 {
-    this->intensity = (float)intensity / 128.0f;
+    this->intensity = intensity / 128.0f;
     size_t bufferLen = streamRate / AGB_FPS;
     bufferPos = 0;
     bufferPos2 = bufferLen;
@@ -22,13 +22,13 @@ ReverbEffect::~ReverbEffect()
 {
 }
 
-void ReverbEffect::ProcessData(float *buffer, size_t nBlocks)
+void ReverbEffect::ProcessData(sample *buffer, size_t numSamples)
 {
-    while (nBlocks > 0)
+    while (numSamples > 0)
     {
-        size_t left = processInternal(buffer, nBlocks);
-        buffer += (nBlocks - left) * N_CHANNELS;
-        nBlocks = left;
+        size_t left = processInternal(buffer, numSamples);
+        buffer += (numSamples - left);
+        numSamples = left;
     }
 }
 
@@ -38,14 +38,14 @@ void ReverbEffect::ProcessData(float *buffer, size_t nBlocks)
 
 size_t ReverbEffect::getBlocksPerBuffer() const
 {
-    return reverbBuffer.size() / N_CHANNELS;
+    return reverbBuffer.size();
 }
 
-size_t ReverbEffect::processInternal(float *buffer, size_t nBlocks)
+size_t ReverbEffect::processInternal(sample *buffer, size_t numSamples)
 {
-    assert(nBlocks > 0);
-    std::vector<float>& rbuf = reverbBuffer;
-    size_t count = std::min(std::min(getBlocksPerBuffer() - bufferPos2, getBlocksPerBuffer() - bufferPos), nBlocks);
+    assert(numSamples > 0);
+    std::vector<sample>& rbuf = reverbBuffer;
+    size_t count = std::min(std::min(getBlocksPerBuffer() - bufferPos2, getBlocksPerBuffer() - bufferPos), numSamples);
     bool reset = false, reset2 = false;
     if (getBlocksPerBuffer() - bufferPos == count) {
         reset = true;
@@ -55,16 +55,17 @@ size_t ReverbEffect::processInternal(float *buffer, size_t nBlocks)
     } 
     size_t c = count;
     do {
-        float rev = (rbuf[bufferPos * 2] + rbuf[bufferPos * 2 + 1] + 
-                rbuf[bufferPos2 * 2] + rbuf[bufferPos2 * 2 + 1]) * intensity * (1.0f / 4.0f);
-        rbuf[bufferPos * 2] = *buffer++ += rev;
-        rbuf[bufferPos * 2 + 1] = *buffer++ += rev;
+        float rev = (rbuf[bufferPos].left + rbuf[bufferPos].right + 
+                rbuf[bufferPos2].left + rbuf[bufferPos2].right) * intensity * (1.0f / 4.0f);
+        rbuf[bufferPos].left  = buffer->left  += rev;
+        rbuf[bufferPos].right = buffer->right += rev;
+        buffer++;
         bufferPos++;
         bufferPos2++;
     } while (--c > 0);
     if (reset2) bufferPos2 = 0;
     if (reset) bufferPos = 0;
-    return nBlocks - count;
+    return numSamples - count;
 }
 
 /*
@@ -73,7 +74,7 @@ size_t ReverbEffect::processInternal(float *buffer, size_t nBlocks)
 
 ReverbGS1::ReverbGS1(uint8_t intensity, size_t streamRate, uint8_t numAgbBuffers)
     : ReverbEffect(intensity, streamRate, numAgbBuffers), 
-    gsBuffer((streamRate / AGB_FPS) * N_CHANNELS, 0.f)
+    gsBuffer((streamRate / AGB_FPS), sample{0.0f, 0.0f})
 {
     bufferPos2 = 0;
 }
@@ -84,15 +85,15 @@ ReverbGS1::~ReverbGS1()
 
 size_t ReverbGS1::getBlocksPerGsBuffer() const
 {
-    return gsBuffer.size() / N_CHANNELS;
+    return gsBuffer.size();
 }
 
-size_t ReverbGS1::processInternal(float *buffer, size_t nBlocks)
+size_t ReverbGS1::processInternal(sample *buffer, size_t numSamples)
 {
-    std::vector<float>& rbuf = reverbBuffer;
+    std::vector<sample>& rbuf = reverbBuffer;
     const size_t bPerBuf = getBlocksPerBuffer();
     const size_t bPerGsBuf = getBlocksPerGsBuffer();
-    size_t count = std::min(std::min(bPerBuf - bufferPos, bPerGsBuf - bufferPos2), nBlocks);
+    size_t count = std::min(std::min(bPerBuf - bufferPos, bPerGsBuf - bufferPos2), numSamples);
     bool reset = false, resetGS = false;
 
     if (count == bPerBuf - bufferPos)
@@ -100,25 +101,24 @@ size_t ReverbGS1::processInternal(float *buffer, size_t nBlocks)
     if (count == bPerGsBuf - bufferPos2)
         resetGS = true;
 
-
     size_t c = count;
     do {
-        float mixL = buffer[0] + gsBuffer[bufferPos2 * 2    ];
-        float mixR = buffer[1] + gsBuffer[bufferPos2 * 2 + 1];
+        float mixL = buffer->left  + gsBuffer[bufferPos2].left;
+        float mixR = buffer->right + gsBuffer[bufferPos2].right;
 
-        float lA = rbuf[bufferPos * 2    ];
-        float rA = rbuf[bufferPos * 2 + 1];
+        float lA = rbuf[bufferPos].left;
+        float rA = rbuf[bufferPos].right;
 
-        buffer[0] = rbuf[bufferPos * 2    ] = mixL;
-        buffer[1] = rbuf[bufferPos * 2 + 1] = mixR;
+        buffer->left  = rbuf[bufferPos].left  = mixL;
+        buffer->right = rbuf[bufferPos].right = mixR;
 
         float lRMix = 0.25f * mixL + 0.25f * rA;
         float rRMix = 0.25f * mixR + 0.25f * lA;
 
-        gsBuffer[bufferPos2 * 2    ] = lRMix;
-        gsBuffer[bufferPos2 * 2 + 1] = rRMix;
+        gsBuffer[bufferPos2].left  = lRMix;
+        gsBuffer[bufferPos2].right = rRMix;
 
-        buffer += 2;
+        buffer++;
 
         bufferPos++;
         bufferPos2++;
@@ -126,7 +126,7 @@ size_t ReverbGS1::processInternal(float *buffer, size_t nBlocks)
 
     if (resetGS) bufferPos2 = 0;
     if (reset) bufferPos = 0;
-    return nBlocks - count;
+    return numSamples - count;
 }
 
 /*
@@ -136,10 +136,10 @@ size_t ReverbGS1::processInternal(float *buffer, size_t nBlocks)
 ReverbGS2::ReverbGS2(uint8_t intensity, size_t streamRate, uint8_t numAgbBuffers,
         float rPrimFac, float rSecFac)
     : ReverbEffect(intensity, streamRate, numAgbBuffers), 
-    gs2Buffer(streamRate / AGB_FPS * N_CHANNELS, 0.f)
+    gs2Buffer(streamRate / AGB_FPS, sample{0.0f, 0.0f})
 {
     // equivalent to the offset of -0xB0 samples for a 0x210 buffer size
-    bufferPos2 = getBlocksPerBuffer() - (gs2Buffer.size() / N_CHANNELS / 3);
+    bufferPos2 = getBlocksPerBuffer() - (gs2Buffer.size() / 3);
     gs2Pos = 0;
     this->rPrimFac = rPrimFac;
     this->rSecFac = rSecFac;
@@ -149,13 +149,13 @@ ReverbGS2::~ReverbGS2()
 {
 }
 
-size_t ReverbGS2::processInternal(float *buffer, size_t nBlocks)
+size_t ReverbGS2::processInternal(sample *buffer, size_t numSamples)
 {
-    assert(nBlocks > 0);
-    std::vector<float>& rbuf = reverbBuffer;
+    assert(numSamples > 0);
+    std::vector<sample>& rbuf = reverbBuffer;
     size_t count = std::min(
             std::min(getBlocksPerBuffer() - bufferPos2, getBlocksPerBuffer() - bufferPos), 
-            std::min(nBlocks, gs2Buffer.size() / N_CHANNELS - gs2Pos)
+            std::min(numSamples, gs2Buffer.size() - gs2Pos)
             );
     bool reset = false, reset2 = false, resetgs2 = false;
 
@@ -171,25 +171,25 @@ size_t ReverbGS2::processInternal(float *buffer, size_t nBlocks)
 
     size_t c = count;
     do {
-        float mixL = buffer[0] + gs2Buffer[gs2Pos * 2    ];
-        float mixR = buffer[1] + gs2Buffer[gs2Pos * 2 + 1];
+        float mixL = buffer->left  + gs2Buffer[gs2Pos].left;
+        float mixR = buffer->right + gs2Buffer[gs2Pos].left;
 
-        float lA = rbuf[bufferPos * 2    ];
-        float rA = rbuf[bufferPos * 2 + 1];
+        float lA = rbuf[bufferPos].left;
+        float rA = rbuf[bufferPos].right;
 
-        buffer[0] = rbuf[bufferPos * 2    ] = mixL;
-        buffer[1] = rbuf[bufferPos * 2 + 1] = mixR;
+        buffer->left  = rbuf[bufferPos].left  = mixL;
+        buffer->right = rbuf[bufferPos].right = mixR;
 
         float lRMix = lA * rPrimFac + rA * rSecFac;
         float rRMix = rA * rPrimFac + lA * rSecFac;
 
-        float lB = rbuf[bufferPos2 * 2 + 1] * 0.25f;
+        float lB = rbuf[bufferPos2].right * 0.25f;
         float rB = mixR * 0.25f;
 
-        gs2Buffer[gs2Pos * 2    ] = lRMix + lB;
-        gs2Buffer[gs2Pos * 2 + 1] = rRMix + rB;
+        gs2Buffer[gs2Pos].left  = lRMix + lB;
+        gs2Buffer[gs2Pos].right = rRMix + rB;
 
-        buffer += 2;
+        buffer++;
 
         bufferPos++;
         bufferPos2++;
@@ -198,7 +198,7 @@ size_t ReverbGS2::processInternal(float *buffer, size_t nBlocks)
     if (reset2) bufferPos2 = 0;
     if (reset) bufferPos = 0;
     if (resetgs2) gs2Pos = 0;
-    return nBlocks - count;
+    return numSamples - count;
 }
 
 /*
@@ -214,11 +214,11 @@ ReverbTest::~ReverbTest()
 {
 }
 
-size_t ReverbTest::processInternal(float *buffer, size_t nBlocks)
+size_t ReverbTest::processInternal(sample *buffer, size_t numSamples)
 {
-    assert(nBlocks > 0);
-    std::vector<float>& rbuf = reverbBuffer;
-    size_t count = std::min(std::min(getBlocksPerBuffer() - bufferPos, getBlocksPerBuffer() - bufferPos2), nBlocks);
+    assert(numSamples > 0);
+    std::vector<sample>& rbuf = reverbBuffer;
+    size_t count = std::min(std::min(getBlocksPerBuffer() - bufferPos, getBlocksPerBuffer() - bufferPos2), numSamples);
     bool reset = false, reset2 = false;
     if (getBlocksPerBuffer() - bufferPos2 == count) {
         reset2 = true;
@@ -229,11 +229,11 @@ size_t ReverbTest::processInternal(float *buffer, size_t nBlocks)
     size_t c = count;
     do {
         const float g = 0.8f;
-        float input_l = buffer[0];
-        float input_r = buffer[1];
+        float input_l = buffer->left;
+        float input_r = buffer->right;
 
-        float feedback_l = rbuf[bufferPos * 2    ];
-        float feedback_r = rbuf[bufferPos * 2 + 1];
+        float feedback_l = rbuf[bufferPos].left;
+        float feedback_r = rbuf[bufferPos].right;
 
         float new_feedback_l = input_l + g * feedback_l;
         float new_feedback_r = input_r + g * feedback_r;
@@ -241,11 +241,12 @@ size_t ReverbTest::processInternal(float *buffer, size_t nBlocks)
         float output_l = -g * new_feedback_l + feedback_l;
         float output_r = -g * new_feedback_r + feedback_r;
 
-        *buffer++ = output_l;
-        *buffer++ = output_r;
+        buffer->left  = output_l;
+        buffer->right = output_r;
+        buffer++;
 
-        rbuf[bufferPos * 2    ] = -new_feedback_l;
-        rbuf[bufferPos * 2 + 1] = -new_feedback_r;
+        rbuf[bufferPos].left  = -new_feedback_l;
+        rbuf[bufferPos].right = -new_feedback_r;
         /*
            float in_delay_1_l = rbuf[bufferPos * 2], in_delay_1_r = rbuf[bufferPos * 2 + 1];
            float in_delay_2_l = rbuf[bufferPos2 * 2], in_delay_2_r = rbuf[bufferPos2 * 2 + 1];
@@ -277,5 +278,5 @@ size_t ReverbTest::processInternal(float *buffer, size_t nBlocks)
     } while (--c > 0);
     if (reset2) bufferPos2 = 0;
     if (reset) bufferPos = 0;
-    return nBlocks - count;
+    return numSamples - count;
 }

Makefile
@@ -1,5 +1,5 @@
 CXX = g++
-CXXFLAGS = -Wall -Wextra -Wconversion -Wunreachable-code -std=c++17 -D NDEBUG -O3 -g
+CXXFLAGS = -Wall -Wextra -Wconversion -Wunreachable-code -std=c++17 -O3 -g
 #CXXFLAGS = -Wall -Wextra -Wconversion -Wunreachable-code -std=c++17 -Og -g -fsanitize=address
 BINARY = agbplay
 LIBS = -lm -lncursesw -pthread -lsndfile -lportaudio -ljsoncpp

src/CGBChannel.cpp
@@ -13,35 +13,17 @@
  * public CGBChannel
  */
 
-CGBChannel::CGBChannel()
+CGBChannel::CGBChannel(uint8_t owner, ADSR env, Note note, uint8_t vol, int8_t pan)
+    : env(env), note(note), owner(owner)
 {
-    this->pos = 0;
-    this->owner = INVALID_OWNER;
-    this->envInterStep = 0;
-    this->envLevel = 0;
-    this->fromEnvLevel = 0;
-    this->envPeak = 0;
-    this->envSustain = 0;
-    this->pan = Pan::CENTER;
-    this->fromPan = Pan::CENTER;
-    this->eState = EnvState::DEAD;
+    this->env.att &= 0x7;
+    this->env.dec &= 0x7;
+    this->env.sus &= 0xF;
+    this->env.rel &= 0x7;
+    SetVol(vol, pan);
 }
 
-void CGBChannel::Init(uint8_t owner, CGBDef def, Note note, ADSR env)
-{
-    this->owner = owner;
-    this->note = note;
-    this->def = def;
-    this->env.att = env.att & 0x7;
-    this->env.dec = env.dec & 0x7;
-    this->env.sus = env.sus & 0xF;
-    this->env.rel = env.rel & 0x7;
-    this->eState = EnvState::INIT;
-    this->pos = 0;
-    this->rs->Reset();
-}
-
-uint8_t CGBChannel::GetOwner()
+uint8_t CGBChannel::GetOwner() const
 {
     return owner;
 }
@@ -98,12 +80,12 @@ ChnVol CGBChannel::getVol()
             (fromPan == Pan::LEFT) ? 0.0f : finalToEnv * (1.0f / 32.0f));
 }
 
-uint8_t CGBChannel::GetMidiKey()
+uint8_t CGBChannel::GetMidiKey() const
 {
     return note.originalKey;
 }
 
-int8_t CGBChannel::GetNoteLength()
+int8_t CGBChannel::GetNoteLength() const
 {
     return note.length;
 }
@@ -144,7 +126,7 @@ bool CGBChannel::TickNote()
     }
 }
 
-EnvState CGBChannel::GetState()
+EnvState CGBChannel::GetState() const
 {
     return eState;
 }
@@ -288,31 +270,20 @@ void CGBChannel::updateVolFade()
  * public SquareChannel
  */
 
-SquareChannel::SquareChannel() : CGBChannel()
+SquareChannel::SquareChannel(uint8_t owner, WaveDuty wd, ADSR env, Note note, uint8_t vol, int8_t pan, int16_t pitch)
+    : CGBChannel(owner, env, note, vol, pan)
 {
-    rs = std::make_unique<BlepResampler>();
-    pat = CGBPatterns::pat_sq50;
-}
+    SetPitch(pitch);
 
-void SquareChannel::Init(uint8_t owner, CGBDef def, Note note, ADSR env)
-{
-    CGBChannel::Init(owner, def, note, env);
-    switch (def.wd) {
-        case WaveDuty::D12:
-            pat = CGBPatterns::pat_sq12;
-            break;
-        case WaveDuty::D25:
-            pat = CGBPatterns::pat_sq25;
-            break;
-        case WaveDuty::D50:
-            pat = CGBPatterns::pat_sq50;
-            break;
-        case WaveDuty::D75:
-            pat = CGBPatterns::pat_sq75;
-            break;
-        default:
-            throw Xcept("Illegal Square Initializer");
-    }
+    static const float *patterns[4] = {
+        CGBPatterns::pat_sq12,
+        CGBPatterns::pat_sq25,
+        CGBPatterns::pat_sq50,
+        CGBPatterns::pat_sq75,
+    };
+
+    this->pat = patterns[static_cast<int>(wd)];
+    this->rs = std::make_unique<BlepResampler>();
 }
 
 void SquareChannel::SetPitch(int16_t pitch)
@@ -320,36 +291,37 @@ void SquareChannel::SetPitch(int16_t pitch)
     freq = 3520.0f * powf(2.0f, float(note.midiKey - 69) * (1.0f / 12.0f) + float(pitch) * (1.0f / 768.0f));
 }
 
-void SquareChannel::Process(float *buffer, size_t nblocks, MixingArgs& args)
+void SquareChannel::Process(sample *buffer, size_t numSamples, MixingArgs& args)
 {
     stepEnvelope();
     if (eState == EnvState::DEAD)
         return;
-    if (nblocks == 0)
+    if (numSamples == 0)
         return;
 
     ChnVol vol = getVol();
     assert(pat);
-    float lVolStep = (vol.toVolLeft - vol.fromVolLeft) * args.nBlocksReciprocal;
-    float rVolStep = (vol.toVolRight - vol.fromVolRight) * args.nBlocksReciprocal;
+    float lVolStep = (vol.toVolLeft - vol.fromVolLeft) * args.samplesPerBufferInv;
+    float rVolStep = (vol.toVolRight - vol.fromVolRight) * args.samplesPerBufferInv;
     float lVol = vol.fromVolLeft;
     float rVol = vol.fromVolRight;
-    float interStep = freq * args.sampleRateReciprocal;
+    float interStep = freq * args.sampleRateInv;
 
     // TODO add sweep functionality
 
-    float outBuffer[nblocks];
+    float outBuffer[numSamples];
 
-    rs->Process(outBuffer, nblocks, interStep, sampleFetchCallback, this);
+    rs->Process(outBuffer, numSamples, interStep, sampleFetchCallback, this);
 
     size_t i = 0;
     do {
         float samp = outBuffer[i++];
-        *buffer++ += samp * lVol;
-        *buffer++ += samp * rVol;
+        buffer->left  += samp * lVol;
+        buffer->right += samp * rVol;
+        buffer++;
         lVol += lVolStep;
         rVol += rVolStep;
-    } while (--nblocks > 0);
+    } while (--numSamples > 0);
 
     updateVolFade();
 }
@@ -374,76 +346,70 @@ bool SquareChannel::sampleFetchCallback(std::vector<float>& fetchBuffer, size_t 
  * public WaveChannel
  */
 
-/* This LUT is currently unused. I's supposed to be more accurate to the original
+/* This LUT is currently unused. It's supposed to be more accurate to the original
  * but on the other hand it just doesn't sound as good as a linear curve */
 uint8_t WaveChannel::volLut[] = {
     0, 0, 4, 4, 4, 4, 8, 8, 8, 8, 12, 12, 12, 12, 16, 16
 };
 
-WaveChannel::WaveChannel() : CGBChannel()
+WaveChannel::WaveChannel(uint8_t owner, const uint8_t *wavePtr, ADSR env, Note note, uint8_t vol, int8_t pan, int16_t pitch)
+    : CGBChannel(owner, env, note, vol, pan)
 {
-    rs = std::make_unique<BlepResampler>();
-    for (int i = 0; i < 32; i++)
-    {
-        waveBuffer[i] = 0.0f;
-    }
-}
+    SetPitch(pitch);
 
-void WaveChannel::Init(uint8_t owner, CGBDef def, Note note, ADSR env)
-{
-    //env.sus = (env.sus * 2) > 0xF ? 0xF : uint8_t(env.sus * 2);
-    CGBChannel::Init(owner, def, note, env);
+    this->rs = std::make_unique<BlepResampler>();
 
+    /* wave samples are unsigned by default, so we'll load them with
+     * DC offset correction */
     float sum = 0.0f;
-    for (size_t i = 0; i < 16; i++)
-    {
-        uint8_t twoNibbles = def.wavePtr[i];
-        float first = float(twoNibbles >> 4) * (1.0f / 16.0f);
+    for (int i = 0; i < 16; i++) {
+        uint8_t twoNibbles = wavePtr[i];
+        float first = static_cast<float>(twoNibbles >> 4) / 16.0f;
         sum += first;
-        float second = float(twoNibbles & 0xF) * (1.0f / 16.0f);
+        float second = static_cast<float>(twoNibbles & 0xF) / 16.0f;
         sum += second;
-        waveBuffer[i*2] = first;
-        waveBuffer[i*2+1] = second;
+        this->waveBuffer[i * 2 + 0] = first;
+        this->waveBuffer[i * 2 + 1] = second;
     }
+
     float dcCorrection = sum * (1.0f / 32.0f);
     for (size_t i = 0; i < 32; i++)
-    {
-        waveBuffer[i] -= dcCorrection;
-    }
+        this->waveBuffer[i] -= dcCorrection;
 }
 
 void WaveChannel::SetPitch(int16_t pitch)
 {
-    // 7040 = 440 * 16
-    freq = 7040.0f * powf(2.0f, float(note.midiKey - 69) * (1.0f / 12.0f) + float(pitch) * (1.0f / 768.0f));
+    freq = (440.0f * 16.0f) * 
+        powf(2.0f, float(note.midiKey - 69) * (1.0f / 12.0f) + float(pitch) * (1.0f / 768.0f));
 }
 
-void WaveChannel::Process(float *buffer, size_t nblocks, MixingArgs& args)
+void WaveChannel::Process(sample *buffer, size_t numSamples, MixingArgs& args)
 {
     stepEnvelope();
     if (eState == EnvState::DEAD)
         return;
-    if (nblocks == 0)
+    if (numSamples == 0)
         return;
     ChnVol vol = getVol();
-    float lVolStep = (vol.toVolLeft - vol.fromVolLeft) * args.nBlocksReciprocal;
-    float rVolStep = (vol.toVolRight - vol.fromVolRight) * args.nBlocksReciprocal;
+    float lVolStep = (vol.toVolLeft - vol.fromVolLeft) * args.samplesPerBufferInv;
+    float rVolStep = (vol.toVolRight - vol.fromVolRight) * args.samplesPerBufferInv;
     float lVol = vol.fromVolLeft;
     float rVol = vol.fromVolRight;
-    float interStep = freq * args.sampleRateReciprocal;
+    float interStep = freq * args.sampleRateInv;
 
-    float outBuffer[nblocks];
+    float outBuffer[numSamples];
 
-    rs->Process(outBuffer, nblocks, interStep, sampleFetchCallback, this);
+    rs->Process(outBuffer, numSamples, interStep, sampleFetchCallback, this);
 
     size_t i = 0;
     do {
         float samp = outBuffer[i++];
-        *buffer++ += samp * lVol;
-        *buffer++ += samp * rVol;
+        buffer->left  += samp * lVol;
+        buffer->right += samp * rVol;
+        buffer++;
         lVol += lVolStep;
         rVol += rVolStep;
-    } while (--nblocks > 0);
+    } while (--numSamples > 0);
 
     updateVolFade();
 }
@@ -468,23 +434,12 @@ bool WaveChannel::sampleFetchCallback(std::vector<float>& fetchBuffer, size_t sa
  * public NoiseChannel
  */
 
-NoiseChannel::NoiseChannel() : CGBChannel()
+NoiseChannel::NoiseChannel(uint8_t owner, NoisePatt np, ADSR env, Note note, uint8_t vol, int8_t pan, int16_t pitch)
+    : CGBChannel(owner, env, note, vol, pan)
 {
-    rs = std::make_unique<NearestResampler>();
-    def.np = NoisePatt::FINE;
-}
-
-void NoiseChannel::Init(uint8_t owner, CGBDef def, Note note, ADSR env)
-{
-    CGBChannel::Init(owner, def, note, env);
-    pos = 0;
-    switch (def.np) {
-        case NoisePatt::ROUGH:
-        case NoisePatt::FINE:
-            break;
-        default:
-            throw Xcept("Illegal Noise Pattern");
-    }
+    SetPitch(pitch);
+    this->rs = std::make_unique<NearestResampler>();
+    this->np = np;
 }
 
 void NoiseChannel::SetPitch(int16_t pitch)
@@ -493,20 +448,20 @@ void NoiseChannel::SetPitch(int16_t pitch)
     freq = std::clamp(noisefreq, 8.0f, 524288.0f);
 }
 
-void NoiseChannel::Process(float *buffer, size_t nblocks, MixingArgs& args)
+void NoiseChannel::Process(sample *buffer, size_t numSamples, MixingArgs& args)
 {
     stepEnvelope();
     if (eState == EnvState::DEAD)
         return;
 
     ChnVol vol = getVol();
-    float lVolStep = (vol.toVolLeft - vol.fromVolLeft) * args.nBlocksReciprocal;
-    float rVolStep = (vol.toVolRight - vol.fromVolRight) * args.nBlocksReciprocal;
+    float lVolStep = (vol.toVolLeft - vol.fromVolLeft) * args.samplesPerBufferInv;
+    float rVolStep = (vol.toVolRight - vol.fromVolRight) * args.samplesPerBufferInv;
     float lVol = vol.fromVolLeft;
     float rVol = vol.fromVolRight;
     float interStep = freq / NOISE_SAMPLING_FREQ;
 
-    float outBuffer[nblocks];
+    float outBuffer[numSamples];
 
     Resampler::ResamplerChainData rcd;
     rcd._this = rs.get();
@@ -514,18 +469,19 @@ void NoiseChannel::Process(float *buffer, size_t nblocks, MixingArgs& args)
     rcd.cbPtr = sampleFetchCallback;
     rcd.cbdata = this;
 
-    srs.Process(outBuffer, nblocks,
+    srs.Process(outBuffer, numSamples,
             NOISE_SAMPLING_FREQ / float(STREAM_SAMPLERATE),
             Resampler::ResamplerChainSampleFetchCB, &rcd);
 
     size_t i = 0;
     do {
         float samp = outBuffer[i++];
-        *buffer++ += samp * lVol;
-        *buffer++ += samp * rVol;
+        buffer->left  += samp * lVol;
+        buffer->right += samp * rVol;
+        buffer++;
         lVol += lVolStep;
         rVol += rVolStep;
-    } while (--nblocks > 0);
+    } while (--numSamples > 0);
 
     updateVolFade();
 }
@@ -539,12 +495,12 @@ bool NoiseChannel::sampleFetchCallback(std::vector<float>& fetchBuffer, size_t s
     size_t i = fetchBuffer.size();
     fetchBuffer.resize(samplesRequired);
 
-    if (_this->def.np == NoisePatt::FINE) {
+    if (_this->np == NoisePatt::FINE) {
         do {
             fetchBuffer[i++] = CGBPatterns::pat_noise_fine[_this->pos++] - 0.5f;
             _this->pos %= NOISE_FINE_LEN;
         } while (--samplesToFetch > 0);
-    } else if (_this->def.np == NoisePatt::ROUGH) {
+    } else if (_this->np == NoisePatt::ROUGH) {
         do {
             fetchBuffer[i++] = CGBPatterns::pat_noise_rough[_this->pos++] - 0.5f;
             _this->pos %= NOISE_ROUGH_LEN;

src/CGBChannel.h
@@ -14,54 +14,52 @@
 class CGBChannel
 {
 public: 
-    CGBChannel();
+    CGBChannel(uint8_t owner, ADSR env, Note note, uint8_t vol, int8_t pan);
     CGBChannel(const CGBChannel&) = delete;
     CGBChannel& operator=(const CGBChannel&) = delete;
+    virtual ~CGBChannel() = default;
 
-    virtual void Init(uint8_t owner, CGBDef def, Note note, ADSR env);
-    virtual void Process(float *buffer, size_t nblocks, MixingArgs& args) = 0;
-    uint8_t GetOwner();
+    virtual void Process(sample *buffer, size_t numSamples, MixingArgs& args) = 0;
+    uint8_t GetOwner() const;
     void SetVol(uint8_t vol, int8_t pan);
-    uint8_t GetMidiKey();
-    int8_t GetNoteLength();
+    uint8_t GetMidiKey() const;
+    int8_t GetNoteLength() const;
     void Release();
     virtual void SetPitch(int16_t pitch) = 0;
     bool TickNote(); // returns true if note remains active
-    EnvState GetState();
+    EnvState GetState() const;
 protected:
     virtual void stepEnvelope();
     void updateVolFade();
     ChnVol getVol();
     enum class Pan { LEFT, CENTER, RIGHT };
-    uint32_t pos;
-    float freq;
+    uint32_t pos = 0;
+    float freq = 0.0f;
     ADSR env;
     Note note;
-    CGBDef def;
-    EnvState eState;
-    EnvState nextState;
-    Pan pan;
+    EnvState eState = EnvState::INIT;
+    EnvState nextState = EnvState::INIT;
+    Pan pan = Pan::CENTER;
     std::unique_ptr<Resampler> rs;
-    uint8_t envInterStep;
-    uint8_t envLevel;
-    uint8_t envPeak;
-    uint8_t envSustain;
-    // these values are always 1 frame behind in order to provide a smooth transition
-    Pan fromPan;
-    uint8_t fromEnvLevel;
+    uint8_t envInterStep = 0;
+    uint8_t envLevel = 0;
+    uint8_t envPeak = 0;
+    uint8_t envSustain = 0;
     uint8_t owner;
+    // these values are always 1 frame behind in order to provide a smooth transition
+    Pan fromPan = Pan::CENTER;
+    uint8_t fromEnvLevel = 0;
 };
 
 class SquareChannel : public CGBChannel
 {
 public:
-    SquareChannel();
+    SquareChannel(uint8_t owner, WaveDuty wd, ADSR env, Note note, uint8_t vol, int8_t pan, int16_t pitch);
 
-    void Init(uint8_t owner, CGBDef def, Note note, ADSR env) override;
     void SetPitch(int16_t pitch) override;
-    void Process(float *buffer, size_t nblocks, MixingArgs& args) override;
+    void Process(sample *buffer, size_t numSamples, MixingArgs& args) override;
 
-    const float *pat;
+    const float *pat = nullptr;
 private:
     static bool sampleFetchCallback(std::vector<float>& fetchBuffer, size_t samplesRequired, void *cbdata);
 };
@@ -69,11 +67,10 @@ private:
 class WaveChannel : public CGBChannel
 {
 public:
-    WaveChannel();
+    WaveChannel(uint8_t owner, const uint8_t *wavePtr, ADSR env, Note note, uint8_t vol, int8_t pan, int16_t pitch);
 
-    void Init(uint8_t owner, CGBDef def, Note note, ADSR env) override;
     void SetPitch(int16_t pitch) override;
-    void Process(float *buffer, size_t nblocks, MixingArgs& args) override;
+    void Process(sample *buffer, size_t numSamples, MixingArgs& args) override;
 private:
     static bool sampleFetchCallback(std::vector<float>& fetchBuffer, size_t samplesRequired, void *cbdata);
     float waveBuffer[32];
@@ -83,12 +80,12 @@ private:
 class NoiseChannel : public CGBChannel
 {
 public:
-    NoiseChannel();
+    NoiseChannel(uint8_t owner, NoisePatt np, ADSR env, Note note, uint8_t vol, int8_t pan, int16_t pitch);
 
-    void Init(uint8_t owner, CGBDef def, Note note, ADSR env) override;
     void SetPitch(int16_t pitch) override;
-    void Process(float *buffer, size_t nblocks, MixingArgs& args) override;
+    void Process(sample *buffer, size_t numSamples, MixingArgs& args) override;
 private:
     static bool sampleFetchCallback(std::vector<float>& fetchBuffer, size_t samplesRequired, void *cbdata);
     SincResampler srs;
+    NoisePatt np;
 };

src/Constants.h
@@ -7,7 +7,6 @@
 
 #define BPM_PER_FRAME 150
 
-#define N_CHANNELS 2
 #define STREAM_SAMPLERATE 48000
 
 #define __STRM_BPSM ((STREAM_SAMPLERATE / 24) - 1)

src/GameConfig.cpp
@@ -15,12 +15,12 @@ GameConfig::GameConfig(const std::vector<std::string>& gameCodes)
     this->gameCodes = gameCodes;
 }
 
-const std::vector<std::string>& GameConfig::GetGameCodes()
+const std::vector<std::string>& GameConfig::GetGameCodes() const
 {
     return gameCodes;
 }
 
-ReverbType GameConfig::GetRevType()
+ReverbType GameConfig::GetRevType() const
 {
     return revType;
 }
@@ -30,7 +30,7 @@ void GameConfig::SetRevType(ReverbType revType)
     this->revType = revType;
 }
 
-ResamplerType GameConfig::GetResTypeFixed()
+ResamplerType GameConfig::GetResTypeFixed() const
 {
     return resTypeFixed;
 }
@@ -40,7 +40,7 @@ void GameConfig::SetResTypeFixed(ResamplerType resType)
     this->resTypeFixed = resType;
 }
 
-ResamplerType GameConfig::GetResType()
+ResamplerType GameConfig::GetResType() const
 {
     return resType;
 }
@@ -50,7 +50,7 @@ void GameConfig::SetResType(ResamplerType resType)
     this->resType = resType;
 }
 
-uint8_t GameConfig::GetPCMVol()
+uint8_t GameConfig::GetPCMVol() const
 {
     return pcmVol;
 }
@@ -60,7 +60,7 @@ void GameConfig::SetPCMVol(uint8_t pcmVol)
     this->pcmVol = std::clamp<uint8_t>(pcmVol, 0, 0xF);
 }
 
-uint8_t GameConfig::GetEngineFreq()
+uint8_t GameConfig::GetEngineFreq() const
 {
     return engineFreq;
 }
@@ -70,7 +70,7 @@ void GameConfig::SetEngineFreq(uint8_t engineFreq)
     this->engineFreq = std::clamp<uint8_t>(engineFreq, 1, 12);
 }
 
-uint8_t GameConfig::GetEngineRev()
+uint8_t GameConfig::GetEngineRev() const
 {
     return engineRev;
 }
@@ -80,7 +80,7 @@ void GameConfig::SetEngineRev(uint8_t engineRev)
     this->engineRev = engineRev;
 }
 
-uint8_t GameConfig::GetTrackLimit()
+uint8_t GameConfig::GetTrackLimit() const
 {
     return trackLimit;
 }
@@ -90,7 +90,7 @@ void GameConfig::SetTrackLimit(uint8_t trackLimit)
     this->trackLimit = std::clamp<uint8_t>(trackLimit, 0, 16);
 }
 
-uint16_t GameConfig::GetRevBufSize()
+uint16_t GameConfig::GetRevBufSize() const
 {
     return revBufSize;
 }

commit 35aa35fa0a62be56964af757de6b061faef7a6bc

fix GS2 reverb regression
Accidentally introduced during refactor in
d3ea650e8413ef07ff602fee77bb30d35d2642c2

This commit fixes it.

src/ReverbEffect.cpp
@@ -172,7 +172,7 @@ size_t ReverbGS2::processInternal(sample *buffer, size_t numSamples)
     size_t c = count;
     do {
         float mixL = buffer->left  + gs2Buffer[gs2Pos].left;
-        float mixR = buffer->right + gs2Buffer[gs2Pos].left;
+        float mixR = buffer->right + gs2Buffer[gs2Pos].right;
 
         float lA = rbuf[bufferPos].left;
         float rA = rbuf[bufferPos].right;
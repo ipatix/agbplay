00f4fe5e7a0cf8f42d620d73cecacb125e142f53
Michael Panzlaff <michael.panzlaff@fau.de>
2021-12-31 12:00:20 +0100

    fix CGB volume calculation
    In Pokemon Emerald Fallarbor Town and Safari Zone the volume of channel
    3 should now always be 100% accurate.

--- src/CGBChannel.cpp
+++ src/CGBChannel.cpp
@@ -286,7 +286,7 @@ void CGBChannel::applyVol()
     else
         this->panCur = Pan::CENTER;
 
-    envPeak = std::clamp<uint8_t>(uint8_t((note.velocity * vol) >> 10), 0, 15);
+    envPeak = std::clamp<uint8_t>(uint8_t(((((note.velocity * vol) >> 7) << 1) - 1) >> 4), 0, 15);
     envSustain = std::clamp<uint8_t>(uint8_t((envPeak * env.sus + 15) >> 4), 0, 15);
     // TODO is this if below right???
     if (envState == EnvState::SUS)

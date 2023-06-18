508907f30a8b7c635faa69d87af7e2aeedd02de2
elysiananthem <glimmerrhodes@gmail.com>
2023-06-18 05:25:47 -0700

    remove terminal checking stuff

--- src/OS.cpp
+++ src/OS.cpp
@@ -100,55 +100,28 @@ const std::filesystem::path OS::GetGlobalConfigDirectory()
 #endif
 
 #ifdef __APPLE__
-#include <libproc.h>
-#include <unistd.h>
+// #include <libproc.h>
 #include <sys/sysctl.h>
+#include <unistd.h>
 
 // http://www.objectpark.net/en/parentpid.html
 
 #define OPProcessValueUnknown UINT_MAX
 
-static int OPParentIDForProcessID(int pid)
-/*" Returns the parent process id
-     for the given process id (pid). "*/
-{
-    struct kinfo_proc info;
-    size_t length = sizeof(struct kinfo_proc);
-    int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, pid };
-    if (sysctl(mib, 4, &info, &length, NULL, 0) < 0)
-        return OPProcessValueUnknown;
-    if (length == 0)
-        return OPProcessValueUnknown;
-    return info.kp_eproc.e_ppid;
-}
+// static int OPParentIDForProcessID(int pid)
+// /*" Returns the parent process id
+//      for the given process id (pid). "*/
+// {
+//     struct kinfo_proc info;
+//     size_t length = sizeof(struct kinfo_proc);
+//     int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, pid };
+//     if (sysctl(mib, 4, &info, &length, NULL, 0) < 0)
+//         return OPProcessValueUnknown;
+//     if (length == 0)
+//         return OPProcessValueUnknown;
+//     return info.kp_eproc.e_ppid;
+// }
 
-// adapted from https://stackoverflow.com/a/8149198
-/*
- * Attempts to check for Apple Terminal.
- * Apple Terminal performs hilariously bad in comparison
- * to iTerm2 to the point where it is almost unusable.
- */
-void OS::CheckTerminal() {
-    pid_t pid = getpid();
-    int ret;
-    pid_t oldpid = getpid();
-    char pathbuf[PROC_PIDPATHINFO_MAXSIZE];
-    do  {
-        ret = proc_pidpath (pid, pathbuf, sizeof(pathbuf));
-        if (strncmp(pathbuf, "/Applications/Utilities/Terminal.app", 37) == 0) {
-            std::cout << "It appears that you are using macOS's built-in Terminal.app." << std::endl << std::endl <<
-                        "This terminal is prone to some serious lag with agbplay." << std::endl <<
-                        "It is recommended to use iTerm2 (https://www.iterm2.com/), as it can" << std::endl <<
-                        "run agbplay without any lag." << std::endl << std::endl <<
-                        "Press Return to continue, or Ctrl+C to exit.";
-            std::cin.ignore();
-            return;
-        }
-        oldpid = pid;
-        pid = (pid_t)OPParentIDForProcessID(pid); } while (ret > 0 && pid > 1 /* launchd */ && pid != oldpid && pid != OPProcessValueUnknown); }
-#else
-void OS::CheckTerminal()
-{
-    return;
-}
+void OS::CheckTerminal() { return; }
+
 #endif

e685b4910e1c1ae7c2cb44d7841884b5114c8434
Michael Panzlaff <michael.panzlaff@fau.de>
2024-04-09 02:32:09 +0200

    add note about MIN_SONG_NUM

--- src/Constants.h
+++ src/Constants.h
@@ -2,7 +2,7 @@
 
 #define NUM_NOTES 128
 #define PROG_UNDEFINED 0xFF
-#define MIN_SONG_NUM 4
+#define MIN_SONG_NUM 4      // lowest seen one is 7 in Tetris World
 
 #define BPM_PER_FRAME 150
 #define AGB_FPS 60

0e7b6ad58617358dd0781b82f14b43bd50c0f02f
Michael Panzlaff <michael.panzlaff@fau.de>
2024-05-25 15:51:36 +0200

    initialize MP2KTrack in separate function
    This will allow the track to be initialized without having to
    reconstruct the object.

--- src/agbplay-nc/MP2KTrack.cpp
+++ src/agbplay-nc/MP2KTrack.cpp
@@ -3,6 +3,40 @@
 MP2KTrack::MP2KTrack(size_t pos)
     : pos(pos)
 {
+    Init();
+}
+
+void MP2KTrack::Init()
+{
+    patternLevel = 0;
+    modt = MODT::PITCH;
+    lastCmd = 0;
+    pitch = 0;
+    lastNoteKey = 0;
+    lastNoteVel = 0;
+    lastNoteLen = 0;
+    reptCount = 0;
+    prog = PROG_UNDEFINED; // TODO replace this with an instrument definition like in original MP2K
+    vol = 100;             // TODO is this correct? original uses 0
+    mod = 0;
+    bendr = 0;
+    priority = 0;
+    lfos = 22;
+    lfodl = 0;
+    lfodlCount = 0;
+    lfoPhase = 0;
+    lfoValue = 0;
+    pseudoEchoVol = 0;
+    pseudoEchoLen = 0;
+    delay = 0;
+    pan = 0;
+    bend = 0;
+    tune = 0;
+    keyShift = 0;
+    muted = false;
+    isRunning = true;
+    updateVolume = false;
+    updatePitch = false;
 }
 
 int16_t MP2KTrack::GetPitch()

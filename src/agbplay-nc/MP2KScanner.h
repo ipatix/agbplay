27e6da235070e14bbf582920af155029f7b74ccd
Michael Panzlaff <michael.panzlaff@fau.de>
2024-06-18 00:15:27 +0200

    continue work on Profile

--- src/agbplay-nc/MP2KScanner.h
+++ src/agbplay-nc/MP2KScanner.h
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <cstddef>
 
+#include "Types.h"
+
 class Rom;
 
 class MP2KScanner {
@@ -11,23 +13,16 @@ public:
     MP2KScanner(const Rom &rom);
 
     struct Result {
-        uint8_t pcm_vol = 0;
-        uint8_t pcm_rev = 0;
-        uint8_t pcm_freq = 0;
-        uint8_t pcm_max_channels = 0;   // not used in agbplay
-        uint8_t dac_config = 0;         // not used in agbplay
-
-        std::vector<std::pair<uint8_t, bool>> player_configs;
-
-        size_t songtable_pos = 0;
-        uint16_t song_count = 0;
+        MP2KSoundMode mp2kSoundMode;
+        PlayerTableInfo playerTableInfo;
+        SongTableInfo songTableInfo;
     };
 
     std::vector<Result> Scan();
 
 private:
     bool FindSongTable(size_t &findStartPos, size_t &songTablePos, uint16_t &songCount) const;
-    bool FindPlayerTable(size_t songTablePos, size_t &playerTablePos, std::vector<std::pair<uint8_t, bool>> &playerConfigs) const;
+    bool FindPlayerTable(size_t songTablePos, size_t &playerTablePos, PlayerTableInfo &playerTableInfo) const;
     bool FindSoundMode(size_t playerTablePos, size_t &soundModePos, uint32_t &soundMode) const;
 
     bool IsPosReferenced(size_t pos) const;
--- src/agbplay-nc/Profile.h
+++ src/agbplay-nc/Profile.h
@@ -1,12 +1,29 @@
 #pragma once
 
 #include <vector>
+#include <optional>
+
+#include "Types.h"
 
 struct Profile {
+    struct PlaylistEntry {
+        std::string name;
+        uint16_t id;
+    };
+
+    struct GameMatch {
+        std::vector<std::string> gameCodes;
+        std::vector<uint8_t> magicBytes;
+    };
+
     std::vector<PlaylistEntry> playlist;
-    size_t songTableId; // abstract this away in its own struct if there are more scanner hints
-    GameMatch gameMatch;
-    MP2KSoundMode mp2kSoundMode;
+    SongTableInfo songTableInfoConfig;
+    SongTableInfo songTableInfoPlayback;
+    PlayerTableInfo playerTableConfig;
+    PlayerTableInfo playerTablePlayback;
+    MP2KSoundMode mp2kSoundModeConfig;
+    MP2KSoundMode mp2kSoundModePlayback;
     AgbplaySoundMode agbplaySoundMode;
+    GameMatch gameMatch;
     // sound hooks / patches, TODO
 };

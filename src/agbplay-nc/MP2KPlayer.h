3401ad90af44d7a4d569d06d548825e1f1e8cca6
Michael Panzlaff <michael.panzlaff@fau.de>
2024-06-27 17:17:24 +0200

    implement usePriority

--- src/agbplay-nc/MP2KPlayer.h
+++ src/agbplay-nc/MP2KPlayer.h
@@ -25,16 +25,18 @@ public:
     int32_t bpmStack = 0;
     uint16_t bpm = 0;
     const uint8_t playerIdx;
+    const bool usePriority;
     bool enabled = false;
     uint8_t tracksUsed = 0;
+    uint8_t reverb = 0;
 
     size_t GetSoundBankPos() const;
-    uint8_t GetReverb() const;
-    uint8_t GetPriority() const;
     size_t GetSongHeaderPos() const;
+    uint8_t GetPriority() const;
+    uint8_t GetReverb() const;
 private:
     size_t songHeaderPos = 0;
     size_t bankPos = 0;
     const uint8_t trackLimit;
-    const bool usePriority; // TODO actually implement the usage of this flag
+    uint8_t priority = 0;
 };
